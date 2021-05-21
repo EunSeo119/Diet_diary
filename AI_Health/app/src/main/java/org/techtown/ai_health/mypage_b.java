e896085afae0e27190dd382efa963de421b25e4c
kimjuhe <58322511+kimjuhe@users.noreply.github.com>
2021-05-21 13:54:23 +0900

    mypage 추가

---
+++ AI_Health/app/src/main/java/org/techtown/ai_health/mypage_b.java
@@ -0,0 +1,14 @@
+package org.techtown.ai_health;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mypage_b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypage_b);
+    }
+}
