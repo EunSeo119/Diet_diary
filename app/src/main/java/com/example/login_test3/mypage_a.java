aeeb1312195c1c9a265f59428a8d7af5ee009c11
eunseo <qnrqjrm03@naver.com>
2021-05-23 17:31:52 +0900

    은서-마이페이지 로그인 로그아웃 연결

--- app/src/main/java/com/example/login_test3/mypage_a.java
+++ app/src/main/java/com/example/login_test3/mypage_a.java
@@ -15,11 +15,20 @@ public class mypage_a extends AppCompatActivity {
         setContentView(R.layout.activity_mypage_a);
 
         Button home = (Button) findViewById(R.id.homebutton);
+        Button login = (Button) findViewById(R.id.join_button);
 
         home.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/login_test3/mypage_b.java
+++ app/src/main/java/com/example/login_test3/mypage_b.java
@@ -2,7 +2,10 @@ package com.example.login_test3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class mypage_b extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class mypage_b extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage_b);
+
+
+        Button logout = (Button) findViewById(R.id.join_button);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), mypage_a.class);
+                startActivity(intent);
+            }
+        });
     }
 }
