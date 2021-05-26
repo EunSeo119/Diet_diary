5bdec4cc936a5f228edeb5f0223fe8d8fcebd503
kimjuhe <58322511+kimjuhe@users.noreply.github.com>
2021-05-27 04:36:33 +0900

    갤러리에서 불러온 이미지 저장

--- AI_Health/app/src/main/java/org/techtown/ai_health/upload.java
+++ AI_Health/app/src/main/java/org/techtown/ai_health/upload.java
@@ -1,20 +1,41 @@
 package org.techtown.ai_health;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+
 public class upload extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private Uri filePath;
+    public String downloadUrl;
+    private ImageView preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +46,8 @@ public class upload extends AppCompatActivity {
         EditText title = (EditText) findViewById(R.id.title);
         EditText content = (EditText) findViewById(R.id.content);
         Button write = (Button) findViewById(R.id.write);
+        Button camera = (Button) findViewById(R.id.camera);
+        preview = (ImageView) findViewById(R.id.iv_preview);
 
         long now = System.currentTimeMillis();
         Date mDate = new Date(now);
@@ -32,6 +55,7 @@ public class upload extends AppCompatActivity {
         write.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                uploadFile();
                 SimpleDateFormat getDate = new SimpleDateFormat("yyyy-MM-dd");
                 SimpleDateFormat getTime = new SimpleDateFormat("hh:mm:ss");
 
@@ -39,13 +63,84 @@ public class upload extends AppCompatActivity {
                 String stime = getTime.format(mDate);
                 String stitle = title.getText().toString();
                 String scontent = content.getText().toString();
-                Post post = new Post(stitle, scontent, sdate, stime);
+                String sImageUrl = downloadUrl;
+                Post post = new Post(stitle, scontent, sdate, stime, sImageUrl);
                 database.child("Post").push().setValue(post);
                 Intent intent = new Intent(getApplicationContext(), MainActivity.class);
                 startActivity(intent);
             }
         });
 
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "이미지를 선택하세요."), 0);
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable @org.jetbrains.annotations.Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            filePath = data.getData();
+            Log.d(TAG, "uri:" + String.valueOf(filePath));
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                preview.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadFile() {
+        if (filePath != null) {
+            final ProgressDialog progressDialog = new ProgressDialog(this);
+            progressDialog.setTitle("업로드중...");
+            progressDialog.show();
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMhh_mmss");
+            Date now = new Date();
+            String filename = formatter.format(now) + ".png";
+
+            StorageReference storageRef = storage.getReferenceFromUrl("gs://ai-hometraining.appspot.com").child("images/" + filename);
+            downloadUrl="gs://ai-hometraining.appspot.com/images/"+filename;
+
+            storageRef.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "업로드 완료!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            progressDialog.dismiss();
+                            Toast.makeText(getApplicationContext(), "업로드 실패!", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            @SuppressWarnings("VisibleForTests")
+                            double progress = (100 * taskSnapshot.getBytesTransferred()) /  taskSnapshot.getTotalByteCount();
+                            progressDialog.setMessage("Uploaded " + ((int) progress) + "% ...");
+                        }
+                    });
+        } else {
+            Toast.makeText(getApplicationContext(), "파일을 먼저 선택하세요.", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
@@ -55,15 +150,16 @@ class Post {
     String content;
     String date;
     String time;
-    String image;
+    String imageUrl;
 
     Post(){}
 
-    Post(String title, String content, String date, String time){
+    Post(String title, String content, String date, String time, String imageUrl){
         this.title = title;
         this.content = content;
         this.date = date;
         this.time = time;
+        this.imageUrl = imageUrl;
     }
 
     public String getTitle()
@@ -97,5 +193,11 @@ class Post {
         this.time= time;
     }
 
+    public String getImageUrl(){
+        return imageUrl;
+    }
+    public void SetImageUrl(String standard){
+        this.imageUrl= standard;
+    }
 
 }
