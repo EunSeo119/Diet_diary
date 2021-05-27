bbad0de65cad7678b3b05754341c638107010652
kimjuhe <58322511+kimjuhe@users.noreply.github.com>
2021-05-27 14:58:57 +0900

    upload 연결

--- app/src/main/java/com/example/login_test3/MainActivity.java
+++ app/src/main/java/com/example/login_test3/MainActivity.java
@@ -91,8 +91,8 @@ public class MainActivity extends AppCompatActivity {
         heart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MycheckActivity.class);
-                startActivity(intent);
+               Intent intent = new Intent(getApplicationContext(), MycheckActivity.class);
+               startActivity(intent);
             }
         });
     }
